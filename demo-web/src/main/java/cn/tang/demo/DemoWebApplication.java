29488711fb388c37534ec9fa3d8f5ac500623a64
tangwenlong <twlmail@163.com>
2019-10-11 20:32:29 +0800

    实现外置tomcat容器；但是控制台乱码问题存在，在core包中添加log4j之后web启动失败；web启动redis问题依然未解决。

--- demo-web/src/main/java/cn/tang/demo/DemoWebApplication.java
+++ demo-web/src/main/java/cn/tang/demo/DemoWebApplication.java
@@ -2,12 +2,34 @@ package cn.tang.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class DemoWebApplication {
+/**
+ * @description web启动类，使用外置容器tomcat，需继承SpringBootServletInitializer
+ * 				重写configure方法。
+ * @author tangwenlong
+ */
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+public class DemoWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoWebApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(DemoWebApplication.class);
+	}
+
+//	//web端口设置
+//	@Bean
+//	public WebServerFactoryCustomizer<ConfigurableWebServerFactory> webServerFactoryCustomizer() {
+//		return (ConfigurableWebServerFactory factory) -> {
+//			factory.setPort(Integer.parseInt(ApolloUtil.getConfig("web.server")));
+//		};
+//	}
 }
--- demo-web/src/main/java/cn/tang/demo/base/StudentController.java
+++ demo-web/src/main/java/cn/tang/demo/base/StudentController.java
@@ -3,6 +3,8 @@ package cn.tang.demo.base;
 import cn.tang.demo.base.service.StudentService;
 import com.alibaba.dubbo.config.annotation.Reference;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,8 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentController {
 
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
     @Reference(version = "1.0")
     private StudentService studentService;
+
+    @RequestMapping("/out")
+    public String out(String name) {
+        logger.info("接收到的消息为==[{}]", name);
+        return name;
+    }
 
     @RequestMapping("/sayHello")
     public String sayHello(String name) {
