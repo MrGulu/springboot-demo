b570e5d30427bac83349421d71f4fb5e7be4e18b
tangwenlong <twlmail@163.com>
2020-05-26 20:45:01 +0800

    RspCodeEnum优化 模板版本v1.1

--- demo-core/src/main/java/cn/tang/demo/enumbean/RspCodeEnum.java
+++ demo-core/src/main/java/cn/tang/demo/enumbean/RspCodeEnum.java
@@ -1,44 +1,37 @@
 package cn.tang.demo.enumbean;
 
 import cn.tang.demo.exception.IBussinessError;
+import cn.tang.demo.utils.StringUtils;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author tangwenlong
  */
+@Slf4j
 public enum RspCodeEnum implements IBussinessError {
 
-    /*
-     * 返回错误信息
-     * */
     //系统公共异常
     ERR99999("系统异常", "99999"),
     ERR99998("必传参数为空,或参数不符合规范！", "99998"),
-    ERR99997("操作数据库异常！", "99997"),
-    ERR99996("接口发送失败！", "99996"),
-    ERR99995("未查到数据！", "99995"),
-    ERR99994("更新数据库失败,请重试！", "99994"),
-    ERR99993("发送HTTP请求失败！", "99993"),
-    ERR99992("发送HTTP [%s]请求失败！url [%s]", "99992"),
-    ERR99991("发送HTTP [%s]请求异常！url [%s]", "99991"),
-    ERR99982("未查到有效数据！", "99984");
-
+    ERR99997("必传参数%s为空,或参数不符合规范！", "99998"),
+    ERR99996("操作数据库异常！", "99997"),
+    ERR99995("接口发送失败！", "99996"),
+    ERR99994("未查到数据！", "99995"),
+    ERR99993("更新数据库失败,请重试！", "99994"),
+    ERR99992("发送HTTP请求失败！", "99993"),
+    ERR99991("发送HTTP [%s]请求异常！url [%s]", "99991");
 
     private String message;
     private String code;
 
-    private RspCodeEnum(String message, String code) {
+    RspCodeEnum(String message, String code) {
         this.message = message;
         this.code = code;
     }
 
-    // 根据code获取message
-    public static String rtnMessage(String code) {
-        for (RspCodeEnum ce : RspCodeEnum.values()) {
-            if (ce.getCode().equals(code)) {
-                return ce.getMessage();
-            }
-        }
-        return null;
+    @Override
+    public String getCode() {
+        return this.code;
     }
 
     @Override
@@ -46,23 +39,25 @@ public enum RspCodeEnum implements IBussinessError {
         return this.message;
     }
 
-
-    @Override
-    public String getCode() {
-        return this.code;
-    }
-
-
     @Override
     public String getMessage(String... details) {
-        String detailsMessage = this.message;
-
-        try {
-            detailsMessage = String.format(this.message, details);
-        } catch (Exception e) {
-
-        }
-        return detailsMessage;
+        return String.format(this.message, (Object[]) details);
     }
 
+    /**
+     * @description 根据code码匹配enum的value值并返回
+     * @param code 需要匹配的code码
+     * @return value 匹配到的enum的value
+     */
+    public static String returnMatchMsg(String code) {
+        if (StringUtils.isEmpty(code)) {
+            throw new IllegalArgumentException("param should not be empty!");
+        }
+        for (RspCodeEnum ce : RspCodeEnum.values()) {
+            if (code.equals(ce.getCode())) {
+                return ce.getMessage();
+            }
+        }
+        return null;
+    }
 }
